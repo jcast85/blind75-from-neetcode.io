5e5c1b2c02d7ed9f67ca46cb881d654512d02789
Juri Castellani <juri.castellani@hotmail.com>
2025-11-28 18:08:11 +0100

    Best time to buy and sell stock problem has been versioned.

---
+++ src/test/java/com/blind75/problems/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,35 @@
+package com.blind75.problems;
+
+import com.blind75.problems.p05_best_time_to_buy_and_sell_stock.BestTimeToBuyAndSellStock;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BestTimeToBuyAndSellStockTest {
+  @Test
+  void testExample() {
+    BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+    int result = bestTimeToBuyAndSellStock.maxProfit(new int[] {10,1,5,6,7,1});
+    Assertions.assertEquals(6, result);
+  }
+
+  @Test
+  void testExample2() {
+    BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+    int result = bestTimeToBuyAndSellStock.maxProfit(new int[] {10,8,7,5,2});
+    Assertions.assertEquals(0, result);
+  }
+
+  @Test
+  void testExample3() {
+    BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+    int result = bestTimeToBuyAndSellStock.maxProfit(new int[] {1,2,11,4,7});
+    Assertions.assertEquals(10, result);
+  }
+
+  @Test
+  void testExample4() {
+    BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+    int result = bestTimeToBuyAndSellStock.maxProfit(new int[] {3,2,6,5,0,3});
+    Assertions.assertEquals(4, result);
+  }
+}
