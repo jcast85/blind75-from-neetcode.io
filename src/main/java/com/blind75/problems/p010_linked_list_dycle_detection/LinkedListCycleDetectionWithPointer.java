1f3707ec8cc1b9ed221dea4ba30a2300018e1bcf
Juri Castellani <juri.castellani@hotmail.com>
2025-12-01 00:56:43 +0100

    Linked list cycle detection best implementation problem test package has been fixed.

---
+++ src/main/java/com/blind75/problems/p010_linked_list_dycle_detection/LinkedListCycleDetectionWithPointer.java
@@ -0,0 +1,22 @@
+package com.blind75.problems.p010_linked_list_dycle_detection;
+
+import com.blind75.problems.common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycleDetectionWithPointer implements LinkedListCycleDetection {
+  @Override
+  public boolean hasCycle(ListNode head) {
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow == fast) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
