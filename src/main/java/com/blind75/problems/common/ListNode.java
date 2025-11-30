d71fb7ae835367284ef4ab8a8ff86c0372190d8f
Juri Castellani <juri.castellani@hotmail.com>
2025-11-30 17:25:30 +0100

    Utility to simplify creation of ListNode has been added.

--- src/main/java/com/blind75/problems/common/ListNode.java
+++ src/main/java/com/blind75/problems/common/ListNode.java
@@ -13,4 +13,15 @@ public class ListNode {
   public ListNode() {}
   public ListNode(int val) { this.val = val; }
   public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+  public static ListNode of(Integer... val) {
+    if(val == null || val.length == 0) {
+      return ListNode.builder().build();
+    }
+    ListNode head = null;
+    for(int i = val.length - 1; i >= 0; i--) {
+      head = ListNode.builder().val(val[i]).next(head).build();
+    }
+    return head;
+  }
 }
