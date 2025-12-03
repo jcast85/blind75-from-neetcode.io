fae2e601027d479e2c90b480851c920261b0df5d
Juri Castellani <juri.castellani@hotmail.com>
2025-12-04 00:30:13 +0100

    Same Binary Tree problem resolution has been implemented.

---
+++ src/test/java/com/blind75/problems/p015_same_binary_tree/SameBinaryTreeTest.java
@@ -0,0 +1,60 @@
+package com.blind75.problems.p015_same_binary_tree;
+
+import com.blind75.problems.common.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class SameBinaryTreeTest {
+
+  private static final List<SameBinaryTree> sameBinaryTreeList = List.of(
+    new SameBinaryTreeFirstTry()
+  );
+
+  private static final List<InputAndOutput> sameBinaryTreeInputAndOutputList = List.of(
+    new DoubleInputAndOutputBuilder<>()
+      .input1(TreeNode.of(1,2,3))
+      .input2(TreeNode.of(1,2,3))
+      .output(true)
+      .build(),
+    new DoubleInputAndOutputBuilder<>()
+      .input1(TreeNode.of(4,7))
+      .input2(TreeNode.of(4,null,7))
+      .output(false)
+      .build(),
+    new DoubleInputAndOutputBuilder<>()
+      .input1(TreeNode.of(1,2,3))
+      .input2(TreeNode.of(1,3,2))
+      .output(false)
+      .build(),
+    new DoubleInputAndOutputBuilder<>()
+      .input1(null)
+      .input2(TreeNode.of(0))
+      .output(false)
+      .build()
+  );
+
+  static Stream<TestConfig<SameBinaryTree>> testConfigs() {
+    Stream.Builder<TestConfig<SameBinaryTree>> streamBuilder = Stream.builder();
+    for (SameBinaryTree sameBinaryTree : sameBinaryTreeList) {
+      for (InputAndOutput sameBinaryTreeInputAndOutput : sameBinaryTreeInputAndOutputList) {
+        streamBuilder.add(new TestConfigBuilder<SameBinaryTree>()
+          .interfaceToTest(sameBinaryTree)
+          .inputAndOutput(sameBinaryTreeInputAndOutput)
+          .build());
+      }
+    }
+    return streamBuilder.build();
+  }
+
+  @ParameterizedTest
+  @MethodSource("testConfigs")
+  void testExample(TestConfig<SameBinaryTree> testConfig) {
+    DoubleInputAndOutput<TreeNode, TreeNode, Boolean> inputAndOutput = (DoubleInputAndOutput) testConfig.inputAndOutput();
+    boolean result = testConfig.interfaceToTest().isSameTree(inputAndOutput.input1(), inputAndOutput.input2());
+    Assertions.assertEquals(inputAndOutput.output(), result);
+  }
+}
