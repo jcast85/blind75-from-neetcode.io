95d7b2c48f43b9e8c3d0e5e62c6d585d60d3c715
Juri Castellani <juri.castellani@hotmail.com>
2025-12-04 01:56:42 +0100

    A field label has been fixed.

--- src/test/java/com/blind75/problems/common/SingleMethodTestConfig.java
+++ src/test/java/com/blind75/problems/common/SingleMethodTestConfig.java
@@ -3,6 +3,6 @@ package com.blind75.problems.common;
 public record SingleMethodTestConfig<I>(I implementationToTest, InputAndOutput inputAndOutput) {
   @Override
   public String toString() {
-    return "InterfaceToTest: " + implementationToTest + ", inputAndOutput: " + inputAndOutput;
+    return "ImplementationToTest: " + implementationToTest + ", inputAndOutput: " + inputAndOutput;
   }
 }
