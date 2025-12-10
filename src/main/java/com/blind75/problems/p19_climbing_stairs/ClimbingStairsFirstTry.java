9d9a1fcc373e462b66c5af04df530dcecad3f2c8
Juri Castellani <juri.castellani@hotmail.com>
2025-12-10 01:20:38 +0100

    climbingStairs problem fist implementation has been adapted to the submit snippet area on the NeetCode.io website.

--- src/main/java/com/blind75/problems/p19_climbing_stairs/ClimbingStairsFirstTry.java
+++ src/main/java/com/blind75/problems/p19_climbing_stairs/ClimbingStairsFirstTry.java
@@ -1,7 +1,5 @@
 package com.blind75.problems.p19_climbing_stairs;
 
-import java.math.BigInteger;
-
 public class ClimbingStairsFirstTry implements ClimbingStairs {
   @Override
   public int climbStairs(int n) {
@@ -10,21 +8,25 @@ public class ClimbingStairsFirstTry implements ClimbingStairs {
     int changeFactorFromBernoulli = 0;
     for(int k = 0; k < n-changeFactorFromBernoulli; k++) {
       changeFactorFromBernoulli = changeFactorFromBernoulli + 1;
-      BigInteger currentValue = BigInteger.ONE;
+      java.math.BigInteger currentValue = java.math.BigInteger.ONE;
       boolean isValueToAdd = false;
       for(int j = 0; j <= k; j++) {
         if(n-j-changeFactorFromBernoulli>0) {
-          currentValue = currentValue.multiply(BigInteger.valueOf(n-j-changeFactorFromBernoulli));
+          currentValue = currentValue.multiply(java.math.BigInteger.valueOf(n-j-changeFactorFromBernoulli));
           isValueToAdd = true;
         }
       }
       if(isValueToAdd) {
-        BigInteger value = currentValue.divide(ClimbingStairsCommonStaticMethods.getFattoriale(k+1));
+        java.math.BigInteger value = currentValue.divide(getFattoriale(k+1));
         System.out.print(" + " + value);
         count += value.intValue();
       }
     }
     System.out.println(" = " + count);
     return count;
+  }
+
+  static java.math.BigInteger getFattoriale(int n) {
+    return n == 0 ? java.math.BigInteger.ONE : java.math.BigInteger.valueOf(n).multiply(getFattoriale(n-1));
   }
 }
