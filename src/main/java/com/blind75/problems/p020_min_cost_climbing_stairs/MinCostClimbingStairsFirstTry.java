59bc570f6390c46afa9c88640185295ae4340614
Juri Castellani <juri.castellani@hotmail.com>
2025-12-15 18:30:14 +0100

    min cost climbing stairs problem implementation with recursion and cache has been added.

---
+++ src/main/java/com/blind75/problems/p020_min_cost_climbing_stairs/MinCostClimbingStairsFirstTry.java
@@ -0,0 +1,103 @@
+package com.blind75.problems.p020_min_cost_climbing_stairs;
+
+import java.util.*;
+
+public class MinCostClimbingStairsFirstTry implements MinCostClimbingStairs {
+  private static int count = 0;
+
+
+  private record IntArrayKey(int[] a) {
+
+    @Override
+      public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof IntArrayKey(int[] a1))) return false;
+        return Arrays.equals(this.a, a1);
+      }
+
+      @Override
+      public int hashCode() {
+        return Arrays.hashCode(a);
+      }
+
+      @Override
+      public String toString() {
+        return Arrays.toString(a);
+      }
+    }
+
+  @Override
+  public int minCostClimbingStairs(int[] cost) {
+    Map<IntArrayKey, List<Integer>> cache = new HashMap<>();
+    List<Integer> steps = minCostClimbingStairs(cost, cache);
+    return getSumOfIndexedValues(cost, steps, 0);
+  }
+
+  private int getSumOfIndexedValues(int[] cost, List<Integer> steps, int offset) {
+    return steps.stream()
+      .map(index -> cost[index + offset])
+      .reduce(0, Integer::sum);
+  }
+
+  private List<Integer> minCostClimbingStairs(int[] cost, Map<IntArrayKey, List<Integer>> cache) {
+    count++;
+    System.out.println(count);
+    List<Integer> result = new ArrayList<>();
+    if(cost.length == 1) {
+      result.add(0);
+      return result;
+    }
+    if(cost.length == 2) {
+      if(cost[0] <= cost[1]) {
+        result.add(0);
+      } else {
+        result.add(1);
+      }
+      return result;
+    }
+    int[] firstToSecondLastValueArray = Arrays.copyOfRange(cost, 0, cost.length - 1);
+    int[] secondToLastValueArray = Arrays.copyOfRange(cost, 1, cost.length);
+
+    IntArrayKey key1 = new IntArrayKey(firstToSecondLastValueArray);
+    IntArrayKey key2 = new IntArrayKey(secondToLastValueArray);
+
+    List<Integer> firstToSecondLastIndexArray;
+    if(cache.containsKey(key1)) {
+      firstToSecondLastIndexArray = new ArrayList<>(cache.get(key1));
+    } else {
+      firstToSecondLastIndexArray = minCostClimbingStairs(firstToSecondLastValueArray, cache);
+      cache.put(key1, firstToSecondLastIndexArray);
+      firstToSecondLastIndexArray = new ArrayList<>(cache.get(key1));
+    }
+
+    List<Integer> secondToLastIndexArray;
+    if(cache.containsKey(key2)) {
+      secondToLastIndexArray = new ArrayList<>(cache.get(key2));
+    } else {
+      secondToLastIndexArray = minCostClimbingStairs(secondToLastValueArray, cache);
+      cache.put(key2, secondToLastIndexArray);
+      secondToLastIndexArray = new ArrayList<>(cache.get(key2));
+    }
+
+    boolean isFirstToUse = secondToLastIndexArray.getFirst() != 0;
+    boolean isLastToUse = firstToSecondLastIndexArray.getLast() != cost.length - 2;
+    int firstToSecondLastCaseSum = getSumOfIndexedValues(cost, firstToSecondLastIndexArray, 0)
+      + (isLastToUse ? cost[cost.length - 1] : 0);
+    int secondToLastCaseSum = getSumOfIndexedValues(cost, secondToLastIndexArray, 1)
+      + (isFirstToUse ? cost[0] : 0);
+    if(firstToSecondLastCaseSum <= secondToLastCaseSum) {
+      if(isLastToUse) {
+        firstToSecondLastIndexArray.add(cost.length - 1);
+      }
+      return new ArrayList<>(firstToSecondLastIndexArray);
+    } else {
+      for (int i=0; i<secondToLastIndexArray.size(); i++) {
+        secondToLastIndexArray.set(i, secondToLastIndexArray.get(i) + 1);
+      }
+      if(isFirstToUse) {
+        secondToLastIndexArray.addFirst(0);
+      }
+      return new ArrayList<>(secondToLastIndexArray);
+    }
+  }
+}
---
+++ src/test/java/com/blind75/problems/p020_min_cost_climbing_stairs/MinCostClimbingStairsTest.java
@@ -0,0 +1,52 @@
+package com.blind75.problems.p020_min_cost_climbing_stairs;
+
+import com.blind75.problems.common.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class MinCostClimbingStairsTest {
+
+  private static final List<MinCostClimbingStairs> minCostClimbingStairsList = List.of(
+    new MinCostClimbingStairsFirstTry()
+  );
+
+  private static final List<InputAndOutput> minCostClimbingStairsInputAndOutputList = List.of(
+    new SingleInputAndOutputBuilder<>()
+      .input(new int[] {1,2,3})
+      .output(2)
+      .build(),
+    new SingleInputAndOutputBuilder<>()
+      .input(new int[] {1,2,1,2,1,1,1})
+      .output(4)
+      .build(),
+    new SingleInputAndOutputBuilder<>()
+      .input(new int[] {2,1,2,1,1})
+      .output(2)
+      .build()
+  );
+
+  static Stream<SingleMethodTestConfig<MinCostClimbingStairs>> testConfigs() {
+    Stream.Builder<SingleMethodTestConfig<MinCostClimbingStairs>> streamBuilder = Stream.builder();
+    for (MinCostClimbingStairs minCostClimbingStairs : minCostClimbingStairsList) {
+      for (InputAndOutput minCostClimbingStairsInputAndOutput : minCostClimbingStairsInputAndOutputList) {
+        streamBuilder.add(new SingleMethodTestConfigBuilder<MinCostClimbingStairs>()
+          .implementationToTest(minCostClimbingStairs)
+          .inputAndOutput(minCostClimbingStairsInputAndOutput)
+          .build());
+      }
+    }
+    return streamBuilder.build();
+  }
+
+  @ParameterizedTest
+  @MethodSource("testConfigs")
+  void testExample(SingleMethodTestConfig<MinCostClimbingStairs> singleMethodTestConfig) {
+    SingleInputAndOutput<int[], Integer> inputAndOutput = (SingleInputAndOutput) singleMethodTestConfig.inputAndOutput();
+    int result = singleMethodTestConfig.implementationToTest().minCostClimbingStairs(inputAndOutput.input());
+    Assertions.assertEquals(inputAndOutput.output(), result);
+  }
+}
