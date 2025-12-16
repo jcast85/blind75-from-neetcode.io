9fa663c5aecced6503b01f055002ff4379e4aff3
Juri Castellani <juri.castellani@hotmail.com>
2025-12-16 19:04:59 +0100

    Single Number problem implementation has been added.

--- src/main/java/com/blind75/problems/p024_single_number/SingleNumberFirstTry.java
+++ src/main/java/com/blind75/problems/p024_single_number/SingleNumberFirstTry.java
@@ -1,8 +1,18 @@
 package com.blind75.problems.p024_single_number;
 
+import java.util.Arrays;
+
 public class SingleNumberFirstTry implements SingleNumber {
   @Override
   public int singleNumber(int[] nums) {
-    return 0;
+    java.util.List<Integer> list = new java.util.ArrayList<>();
+    for (int num : nums) {
+      if (!list.contains(num)) {
+        list.add(num);
+      } else {
+        list.remove((Integer) num);
+      }
+    }
+    return list.get(0);
   }
 }
