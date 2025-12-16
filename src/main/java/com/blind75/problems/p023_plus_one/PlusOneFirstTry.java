7942ce0ab8c43ce226cc831b4c6f70b9a82a0da0
Juri Castellani <juri.castellani@hotmail.com>
2025-12-16 18:54:35 +0100

    Plus One problem implementation has been added.

--- src/main/java/com/blind75/problems/p023_plus_one/PlusOneFirstTry.java
+++ src/main/java/com/blind75/problems/p023_plus_one/PlusOneFirstTry.java
@@ -3,6 +3,18 @@ package com.blind75.problems.p023_plus_one;
 public class PlusOneFirstTry implements PlusOne {
   @Override
   public int[] plusOne(int[] digits) {
-    return new int[0];
+    for(int i = digits.length - 1; i >= 0; i--) {
+      if(digits[i] < 9) {
+        digits[i] = digits[i] + 1;
+        return digits;
+      } else {
+        digits[i] = 0;
+      }
+    }
+
+    return java.util.stream.IntStream
+      .concat(java.util.stream.IntStream.of(1),
+        java.util.stream.IntStream.of(digits))
+      .toArray();
   }
 }
