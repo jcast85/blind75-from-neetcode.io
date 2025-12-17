ce42bee0dfe2355f9e30e5d667ba6a31b46d7250
Juri Castellani <juri.castellani@hotmail.com>
2025-12-17 09:08:35 +0100

    Number of one bits problem implementation with mod has been added.

---
+++ src/main/java/com/blind75/problems/p025_number_of_one_bits/NumberOfOneBits.java
@@ -0,0 +1,5 @@
+package com.blind75.problems.p025_number_of_one_bits;
+
+public interface NumberOfOneBits {
+  int hammingWeight(int n);
+}
---
+++ src/test/java/com/blind75/problems/p025_number_of_one_bits/NumberOfOneBitsTest.java
@@ -0,0 +1,47 @@
+package com.blind75.problems.p025_number_of_one_bits;
+
+import com.blind75.problems.common.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class NumberOfOneBitsTest {
+
+  private static final List<NumberOfOneBits> numberOfOneBitsList = List.of(
+    new NumberOfOneBitsWithMod()
+  );
+
+  private static final List<InputAndOutput> numberOfOneBitsInputAndOutputList = List.of(
+    new SingleInputAndOutputBuilder<>()
+      .input(23)
+      .output(4)
+      .build(),
+    new SingleInputAndOutputBuilder<>()
+      .input(2147483645)
+      .output(30)
+      .build()
+  );
+
+  static Stream<SingleMethodTestConfig<NumberOfOneBits>> testConfigs() {
+    Stream.Builder<SingleMethodTestConfig<NumberOfOneBits>> streamBuilder = Stream.builder();
+    for (NumberOfOneBits numberOfOneBits : numberOfOneBitsList) {
+      for (InputAndOutput numberOfOneBitsInputAndOutput : numberOfOneBitsInputAndOutputList) {
+        streamBuilder.add(new SingleMethodTestConfigBuilder<NumberOfOneBits>()
+          .implementationToTest(numberOfOneBits)
+          .inputAndOutput(numberOfOneBitsInputAndOutput)
+          .build());
+      }
+    }
+    return streamBuilder.build();
+  }
+
+  @ParameterizedTest
+  @MethodSource("testConfigs")
+  void testExample(SingleMethodTestConfig<NumberOfOneBits> singleMethodTestConfig) {
+    SingleInputAndOutput<Integer, Integer> inputAndOutput = (SingleInputAndOutput) singleMethodTestConfig.inputAndOutput();
+    Integer result = singleMethodTestConfig.implementationToTest().hammingWeight(inputAndOutput.input());
+    org.hamcrest.MatcherAssert.assertThat(result, org.hamcrest.Matchers.equalTo(inputAndOutput.output()));
+  }
+}
