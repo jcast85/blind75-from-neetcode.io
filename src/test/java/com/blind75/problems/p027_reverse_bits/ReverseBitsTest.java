d34ab9c8deffd106834720f3525f9ade03578b5b
Juri Castellani <juri.castellani@hotmail.com>
2025-12-19 18:22:55 +0100

    Tests have been improved.

--- src/test/java/com/blind75/problems/p027_reverse_bits/ReverseBitsTest.java
+++ src/test/java/com/blind75/problems/p027_reverse_bits/ReverseBitsTest.java
@@ -16,27 +16,27 @@ public class ReverseBitsTest {
 
   private static final List<InputAndOutput> reverseBitsInputAndOutputList = List.of(
     new SingleInputAndOutputBuilder<>()
-      .input(21)
-      .output(-671088640)
+      .input(21)            // 00000000000000000000000000010101
+      .output(-1476395008)  // 10101000000000000000000000000000
       .build(),
     new SingleInputAndOutputBuilder<>()
-      .input(43261596)
-      .output(964176192)
+      .input(43261596)      // 00000010100101000001111010011100
+      .output(964176192)    // 00111001011110000010100101000000
       .build(),
     new SingleInputAndOutputBuilder<>()
-      .input(2147483644)
-      .output(1073741822)
+      .input(2147483644)    // 01111111111111111111111111111100
+      .output(1073741822)   // 00111111111111111111111111111110
       .build(),
     new SingleInputAndOutputBuilder<>()
-      .input(-2147483645) // 10000000000000000000000000000011
-      .output(-1073741823) // 11000000000000000000000000000001
+      .input(-2147483645)   // 10000000000000000000000000000011
+      .output(-1073741823)  // 11000000000000000000000000000001
       .build()
   );
 
   static Stream<SingleMethodTestConfig<ReverseBits>> testConfigs() {
     Stream.Builder<SingleMethodTestConfig<ReverseBits>> streamBuilder = Stream.builder();
     for (ReverseBits reverseBits : reverseBitsList) {
-      for (InputAndOutput reverseBitsInputAndOutput : reverseBitsInputAndOutputList.subList(3, 4)) {
+      for (InputAndOutput reverseBitsInputAndOutput : reverseBitsInputAndOutputList) {
         streamBuilder.add(new SingleMethodTestConfigBuilder<ReverseBits>()
           .implementationToTest(reverseBits)
           .inputAndOutput(reverseBitsInputAndOutput)
