3227280eeda66e553030848aa87531d608e4e82c
Juri Castellani <juri.castellani@hotmail.com>
2025-12-19 23:04:27 +0100

    sliding window problems have been moved into a specific package

--- src/test/java/com/blind75/problems/sliding_window/p005_best_time_to_buy_and_sell_stock/BestTimeToBuyAndSellStockTest.java
+++ src/test/java/com/blind75/problems/sliding_window/p005_best_time_to_buy_and_sell_stock/BestTimeToBuyAndSellStockTest.java
@@ -1,4 +1,4 @@
-package com.blind75.problems.bit_manipulation.p005_best_time_to_buy_and_sell_stock;
+package com.blind75.problems.sliding_window.p005_best_time_to_buy_and_sell_stock;
 
 import com.blind75.problems.common.*;
 import org.junit.jupiter.params.ParameterizedTest;
