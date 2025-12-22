0c9a0d58f448f1db091aa88982f33a46da5f5608
Juri Castellani <juri.castellani@hotmail.com>
2025-12-22 06:55:15 +0100

    Valid Sudoki problem naive implementation has been added.

--- src/main/java/com/blind75/problems/arrays_and_hashing/valid_sudoki/ValidSudokuNaive.java
+++ src/main/java/com/blind75/problems/arrays_and_hashing/valid_sudoki/ValidSudokuNaive.java
@@ -3,7 +3,62 @@ package com.blind75.problems.arrays_and_hashing.valid_sudoki;
 public class ValidSudokuNaive implements ValidSudoku {
   @Override
   public boolean isValidSudoku(char[][] board) {
+    for (char[] chars : board) {
+      boolean validSudokuRow = isValidSudokuBlock(chars);
+      if (!validSudokuRow) {
+        return false;
+      }
+    }
+    for(int i=0; i<board.length; i++) {
+      char[] sudokuColumn = getColumn(board, i);
+      boolean validSudokuColumn = isValidSudokuBlock(sudokuColumn);
+      if(!validSudokuColumn) {
+        return false;
+      }
+    }
+    for(int i=0; i<3; i++) {
+      for(int j=0; j<3; j++) {
+        char[] sudokuSubBox = getSubBox(board, i, j);
+        boolean validSudokuSubBox = isValidSudokuBlock(sudokuSubBox);
+        if(!validSudokuSubBox) {
+          return false;
+        }
+      }
+    }
 
-    return false;
+    return true;
+  }
+
+  private boolean isValidSudokuBlock(char[] chars) {
+    boolean[] isRelatedDigitPresent = new boolean[] {false, false, false, false, false, false, false, false, false};
+    for (char aChar : chars) {
+      int numericValue = Character.getNumericValue(aChar);
+      if (numericValue < 0) {
+        continue;
+      }
+      if (isRelatedDigitPresent[numericValue - 1]) {
+        return false;
+      }
+      isRelatedDigitPresent[numericValue - 1] = true;
+    }
+    return true;
+  }
+
+  private char[] getColumn(char[][] board, int columnIndex) {
+    char[] sudokuColumn = new char[9];
+    for (int i=0; i<board.length; i++) {
+      sudokuColumn[i] = board[i][columnIndex];
+    }
+    return sudokuColumn;
+  }
+
+  private char[] getSubBox(char[][] board, int verticalIndex, int horizontalIndex) {
+    char[] sudokuSubBox = new char[9];
+    for(int i=0; i<3; i++) {
+      for(int j=0; j<3; j++) {
+        sudokuSubBox[i+j] = board[3*horizontalIndex+i][3*verticalIndex+j];
+      }
+    }
+    return sudokuSubBox;
   }
 }
