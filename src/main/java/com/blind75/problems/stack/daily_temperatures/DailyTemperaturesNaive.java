e4d42081f8c7a31395f64c80e4e0a85743167b8e
Juri Castellani <juri.castellani@hotmail.com>
2025-12-30 05:19:58 +0100

    Daily Temperatures problem naive implementation has been added.

--- src/main/java/com/blind75/problems/stack/daily_temperatures/DailyTemperaturesNaive.java
+++ src/main/java/com/blind75/problems/stack/daily_temperatures/DailyTemperaturesNaive.java
@@ -1,8 +1,39 @@
 package com.blind75.problems.stack.daily_temperatures;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DailyTemperaturesNaive implements DailyTemperatures {
   @Override
   public int[] dailyTemperatures(int[] temperatures) {
-    return new int[0];
+    List<TemperatureData> temperatureDataList = new ArrayList<>();
+    int[] result = new int[temperatures.length];
+    for(int i=temperatures.length-1; i>=0; i--) {
+      if(i>0) {
+        if (temperatures[i] >= temperatures[i - 1]) {
+          temperatureDataList.add(new TemperatureData(i, temperatures[i]));
+        }
+      }
+      for(int j=temperatureDataList.size()-1; j>=0; j--) {
+        if(temperatureDataList.get(j).value>temperatures[i]
+          && temperatureDataList.get(j).index > i
+        ) {
+          result[i] = temperatureDataList.get(j).index - i;
+          break;
+        }
+      }
+
+    }
+    return result;
+  }
+
+  private static class TemperatureData {
+    final int index;
+    final int value;
+
+    private TemperatureData(int index, int value) {
+      this.index = index;
+      this.value = value;
+    }
   }
 }
