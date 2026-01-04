89f22a2a403968914dd027b47ae3afb6aa357786
Juri Castellani <juri.castellani@hotmail.com>
2026-01-05 00:04:13 +0100

    useless stuff has been removed.

--- src/main/java/com/blind75/problems/stack/largest_rectangle_in_histogram/LargestRectangleInHistogramNaive.java
+++ src/main/java/com/blind75/problems/stack/largest_rectangle_in_histogram/LargestRectangleInHistogramNaive.java
@@ -7,27 +7,25 @@ public class LargestRectangleInHistogramNaive implements LargestRectangleInHisto
   public int largestRectangleArea(int[] heights) {
     int largestRectangleArea = 0;
     Stack<RectangleData> rectangleDataStack = new Stack<>();
-    rectangleDataStack.push(new RectangleData(0, 0, 0, false));
+    rectangleDataStack.push(new RectangleData(0, 0, 0));
     for(int i=1; i<heights.length; i++) {
       if(heights[i] < heights[i-1]) {
         int newStartIndex = 0;
         while(!rectangleDataStack.isEmpty() && heights[rectangleDataStack.peek().indexMaxHeight] > heights[i]) {
           RectangleData pop = rectangleDataStack.pop();
           pop.endIndex = i-1;
-          pop.isClosed = true;
           largestRectangleArea = Math.max(largestRectangleArea, RectangleData.getArea(pop, heights));
           newStartIndex = pop.startIndex;
         }
-        rectangleDataStack.push(new RectangleData(newStartIndex, i, i, false));
+        rectangleDataStack.push(new RectangleData(newStartIndex, i, i));
       }
       if(heights[i] > heights[i-1]) {
-        rectangleDataStack.push(new RectangleData(i, i, i, false));
+        rectangleDataStack.push(new RectangleData(i, i, i));
       }
     }
     while(!rectangleDataStack.isEmpty()) {
       RectangleData pop = rectangleDataStack.pop();
       pop.endIndex = heights.length-1;
-      pop.isClosed = true;
       largestRectangleArea = Math.max(largestRectangleArea, RectangleData.getArea(pop, heights));
     }
     return largestRectangleArea;
@@ -37,13 +35,11 @@ public class LargestRectangleInHistogramNaive implements LargestRectangleInHisto
     int startIndex;
     int endIndex;
     int indexMaxHeight;
-    boolean isClosed;
 
-    public RectangleData(int startIndex, int endIndex, int indexMaxHeight, boolean isClosed) {
+    public RectangleData(int startIndex, int endIndex, int indexMaxHeight) {
       this.startIndex = startIndex;
       this.endIndex = endIndex;
       this.indexMaxHeight = indexMaxHeight;
-      this.isClosed = isClosed;
     }
 
     @Override
@@ -52,7 +48,6 @@ public class LargestRectangleInHistogramNaive implements LargestRectangleInHisto
         "startIndex=" + startIndex +
         ", endIndex=" + endIndex +
         ", indexMaxHeight=" + indexMaxHeight +
-        ", isClosed=" + isClosed +
         '}';
     }
 
